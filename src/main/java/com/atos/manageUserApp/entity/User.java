9f570a098418f55a32caaa038ebee5b1275a15fc
foula <fdiallo@ces-incendie.fr>
2021-08-23 16:53:37 +0200

    add: User entity

---
+++ src/main/java/com/atos/manageUserApp/entity/User.java
@@ -0,0 +1,127 @@
+package com.atos.manageUserApp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private Date birthdate;
+    private String country;
+    private String phoneNumber;
+    private String gender;
+
+    /*
+    private enum gender{
+        Male,
+        Female,
+        Other
+    }
+    */
+
+    /**
+     *
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+    
+    /**
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return birthdate
+     */
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    /**
+     *
+     * @param birthdate
+     */
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    /**
+     *
+     * @return gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     *
+     * @param gender
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     *
+     * @return country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     *
+     * @param country
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     *
+     * @return phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     *
+     * @param phoneNumber
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+}
